ccdb35f0225597bf501615b60179136a924bb28e
Yuntai Kyong <yuntai.kyong@gmail.com>
2017-03-01 17:58:27 +0900

    add floyd_requirements.txt

---
+++ README.floyd.md
@@ -0,0 +1,5 @@
+Large-scale CelebFaces Attributes (CelebA) Dataset
+http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html
+
+download link:
+https://drive.google.com/uc?export=download&?id=0B7EVK8r0v71pZjFTYXZWM3FlRnMbuild.tar.xz
--- setup.sh
+++
@@ -1,5 +0,0 @@
-wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
-tar xf ffmpeg-release-64bit-static.tar.xz
-cp ffmpeg-3.2.4-64bit-static/ffmpeg /bin
-rm -rf ffmpeg-3.2.4-64bit-static
-rm ffmpeg-release-64bit-static.tar.xz
